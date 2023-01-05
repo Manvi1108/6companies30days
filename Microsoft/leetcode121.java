bdf8f8d52e68fb1965fe37c84714bf1eba9981d1
Manvi1108 <manvisinghal285@gmail.com>
2023-01-05 17:44:36 +0530

    first commit

---
+++ Microsoft/leetcode121.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+
+public class leetcode121
+{
+    public int maxProfit(int[] prices)
+    {
+        int n = prices.length;
+        int buy = Integer.MAX_VALUE;
+        int maxProfit = Integer.MIN_VALUE;
+        for(int i = 0; i<n; i++)
+        {
+            buy = Math.min(buy,prices[i]);
+            maxProfit = Math.max(maxProfit,prices[i]-buy);
+        }
+        return maxProfit;
+    }
+
+}
